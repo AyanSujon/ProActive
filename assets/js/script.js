3a682055de1ac5763d349bfc2554a03a301066a5
Ayan Sujon <73520526+AyanSujon@users.noreply.github.com>
2023-12-18 17:14:20 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,141 @@
+
+// Scroll Menu Hidden
+let scrollOne = window.pageYOffset;
+window.onscroll = function(){
+  let scrollTwo = window.pageYOffset;
+  if(scrollOne > scrollTwo){
+    document.querySelector('.main_menu').style.top = "0";
+  }else{
+    document.querySelector('.main_menu').style.top = "-100px";
+  }
+  scrollOne = scrollTwo;
+}
+
+
+// #INSIDE OUR APP -> Slider
+// Swiper Slider
+var swiper = new Swiper(".screenshot_slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+// #WHAT OUR CLIENTS SAY’S Slider
+$('.card_wrapper').slick({
+  dots: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows:false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows:false,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+// OUR PRICING PLANS - Pricing Table
+let priceToggler = document.querySelector('.price_toggler');
+let price = document.querySelectorAll('.price');
+let duration = document.querySelectorAll('.duration');
+
+priceToggler.onclick = () =>{
+  priceToggler.classList.toggle('active');
+
+  if(priceToggler.classList.contains('active')){
+    
+    for(var i = 0; i < duration.length; i++)
+    duration[i].innerText = '/ Annual';
+
+    for(var k = 0; k < price.length; k++)
+    price[k].innerText = price[k].innerText * 10;
+  }
+  
+  else{
+
+    for(var i = 0; i < duration.length; i++)
+    duration[i].innerText = '/ Monthly';
+
+    for(var k = 0; k < price.length; k++)
+    price[k].innerText = price[k].innerText / 10;
+  }
+}
+
+// Statistic - Counter-Up2 v2.0.2 
+const counterUp = window.counterUp.default;
+
+const initializeCounter = (statisticElement) => {
+  const counterElement = statisticElement.querySelector('.counter');
+  const IO = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !statisticElement.classList.contains('is-visible')) {
+        counterUp(counterElement, {
+          duration: 2000,
+          delay: 16,
+        });
+        statisticElement.classList.add('is-visible');
+      }
+    });
+  }, { threshold: 1 });
+
+  IO.observe(statisticElement);
+};
+
+// Call the initializeCounter function for each statistic
+document.querySelectorAll('.statistic_container > div').forEach(statisticElement => {
+  initializeCounter(statisticElement);
+});
+
+// Back to Top Button
+const scrollTop = document.querySelector('.scrollTop');
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200){
+    scrollTop.style.display = "flex"
+  }else{
+    scrollTop.style.display = "none"
+  }
+}
+// Scroll to top
+function scrollToTop(){
+  window.scrollTo(0, 0);
+}
